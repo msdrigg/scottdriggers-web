b0e241f341f67b5c6a27344c437bde035176d015
Scott Driggers <scottdriggers@Mac.tailace95.ts.net>
2026-06-21 12:11:35 -0400

    Updating home

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,162 +7,142 @@ import { Timeline, TimelineItem } from "../components/Timeline/Timeline";
 import { ReactNode } from "react";
 
 export default function Home(): ReactNode {
-    return (
-        <Layout
-            title={`Hello from Scott Driggers`}
-            description="Personal webpage for Scott Driggers"
+  return (
+    <Layout
+      title={`Hello from Scott Driggers`}
+      description="Personal webpage for Scott Driggers"
+    >
+      <div>
+        <header
+          className={clsx(
+            "hero hero--primary shadow--lw margin--lg",
+            styles.heroBanner,
+          )}
         >
-            <div>
-                <header
-                    className={clsx(
-                        "hero hero--primary shadow--lw margin--lg",
-                        styles.heroBanner
-                    )}
-                >
-                    Hi, I'm Scott Driggers
-                </header>
+          Hi, I'm Scott Driggers
+        </header>
+      </div>
+      <main className="px-8 py-4 space-y-8">
+        <section>
+          <Heading as="h2" className="text--left">
+            Background
+          </Heading>
+          <p>
+            I graduated from Clemson University with degrees in physics and
+            mathematics. In college I focused on the theoretical side of these
+            fields and studied subjects that ranged from quantum mechanics to
+            abstract algebra.
+          </p>
+          <p>
+            On top of these projects, I done some work in full stack development
+            for saas systems in healthcare, enjoy writing in Rust when I can,
+            built a few mobile apps with bluetooth features, experience in
+            bluetooth development, experience in data science + visualization,
+            experience in standard ml
+          </p>
+          <p>
+            Love learning and what's next for me is learning to manage people,
+            direct a project to completion and work more about the business side
+            of things.
+          </p>
+          <p>
+            Additionally I am hoping to get the chance to learn more on the
+            hardware side (embedded systems, chip design, etc.)
+          </p>
+        </section>
+
+        <section>
+          <Heading as="h2" className="text-left">
+            Experience
+          </Heading>
+          <Timeline>
+            <TimelineItem date="2022-Present">
+              <Heading as="h2">Systems Architect</Heading>
+              <Heading as="h3" className={styles.cardHeader}>
+                Poltys Inc.
+              </Heading>
+              <p>Lead engineer on Location Based Services project.</p>
+              <p>
+                Team lead on mobile healthcare application and primary backend
+                engineer on the project
+              </p>
+            </TimelineItem>
+            <TimelineItem date="2020-2022">
+              <Heading as="h1">Data Scientist · Software Engineer</Heading>
+              <Heading as="h3" className={styles.cardHeader}>
+                Poltys Inc.
+              </Heading>
+              <p>
+                Developed machine learning models for fall risk. Developed
+                algorithms for indoor location tracking.
+              </p>
+            </TimelineItem>
+
+            <TimelineItem date="Summer 2020">
+              <Heading as="h1">Research Intern</Heading>
+              <Heading as="h3" className={styles.cardHeader}>
+                Oak Ridge National Lab
+              </Heading>
+              <p>
+                Applied machine learning models to improve the performance of
+                physics simulations.
+              </p>
+            </TimelineItem>
+
+            <TimelineItem date="Summer 2019">
+              <Heading as="h1">Research Intern</Heading>
+              <Heading as="h3" className={styles.cardHeader}>
+                Clemson University - SIN Group
+              </Heading>
+              <p>
+                Worked on a project to deflect an ion beam in a strong vacuum
+                using a 3D printed deflector.
+              </p>
+              <p>
+                Developed control software to the temperature control system for
+                the vacuum chamber, and wrote visualization software for the ion
+                scattering simulation software.
+              </p>
+            </TimelineItem>
+
+            <TimelineItem date="2018-2021">
+              <Heading as="h1">B.S. Physics and B.S. Mathematics</Heading>
+              <Heading as="h3" className={styles.cardHeader}>
+                Clemson University
+              </Heading>
+              <p>
+                Focused on computational and theoretical physics as well as
+                not-very-applied mathematics.
+              </p>
+            </TimelineItem>
+          </Timeline>
+        </section>
+        <section>
+          <Heading as="h2" className="text--left">
+            Fun facts
+          </Heading>
+          <div className="grow text-center grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
+            <div className={clsx("card", styles.card, "shadow--md")}>
+              <div className="card__header">
+                <Heading as={"h3"}>Home</Heading>
+              </div>
+              <div className="card__body">
+                <p>I'm currently living in Columbia SC</p>
+              </div>
             </div>
-            <main className="px-8 py-4 space-y-8">
-                <section>
-                    <Heading as="h2" className="text--left">
-                        Background
-                    </Heading>
-                    <p>
-                        I graduated from Clemson University with degrees in
-                        physics and mathematics. In college I focused on the
-                        theoretical side of these fields and studied subjects
-                        that ranged from quantum mechanics to abstract algebra.
-                    </p>
-                    <p>
-                        On top of these projects, I done some work in full stack
-                        development for saas systems in healthcare, enjoy
-                        writing in Rust when I can, built a few mobile apps with
-                        bluetooth features, experience in bluetooth development,
-                        experience in data science + visualization, experience
-                        in standard ml
-                    </p>
-                    <p>
-                        Love learning and what's next for me is learning to
-                        manage people, direct a project to completion and work
-                        more about the business side of things.
-                    </p>
-                    <p>
-                        Additionally I am hoping to get the chance to learn more
-                        on the hardware side (embedded systems, chip design,
-                        etc.)
-                    </p>
-                </section>
-
-                <section>
-                    <Heading as="h2" className="text-left">
-                        Experience
-                    </Heading>
-                    <Timeline>
-                        <TimelineItem date="2022-Present">
-                            <Heading as="h2">Systems Architect</Heading>
-                            <Heading as="h3" className={styles.cardHeader}>
-                                Poltys Inc.
-                            </Heading>
-                            <p>
-                                Lead engineer on Location Based Services
-                                project.
-                            </p>
-                            <p>
-                                Team lead on mobile healthcare application and
-                                primary backend engineer on the project
-                            </p>
-                        </TimelineItem>
-                        <TimelineItem date="2020-2022">
-                            <Heading as="h1">
-                                Data Scientist · Software Engineer
-                            </Heading>
-                            <Heading as="h3" className={styles.cardHeader}>
-                                Poltys Inc.
-                            </Heading>
-                            <p>
-                                Developed machine learning models for fall risk.
-                                Developed algorithms for indoor location
-                                tracking.
-                            </p>
-                        </TimelineItem>
-
-                        <TimelineItem date="Summer 2020">
-                            <Heading as="h1">Research Intern</Heading>
-                            <Heading as="h3" className={styles.cardHeader}>
-                                Oak Ridge National Lab
-                            </Heading>
-                            <p>
-                                Applied machine learning models to improve the
-                                performance of physics simulations.
-                            </p>
-                        </TimelineItem>
-
-                        <TimelineItem date="Summer 2019">
-                            <Heading as="h1">Research Intern</Heading>
-                            <Heading as="h3" className={styles.cardHeader}>
-                                Clemson University - SIN Group
-                            </Heading>
-                            <p>
-                                Worked on a project to deflect an ion beam in a
-                                strong vacuum using a 3D printed deflector.
-                            </p>
-                            <p>
-                                Developed control software to the temperature
-                                control system for the vacuum chamber, and wrote
-                                visualization software for the ion scattering
-                                simulation software.
-                            </p>
-                        </TimelineItem>
-
-                        <TimelineItem date="2018-2021">
-                            <Heading as="h1">
-                                B.S. Physics and B.S. Mathematics
-                            </Heading>
-                            <Heading as="h3" className={styles.cardHeader}>
-                                Clemson University
-                            </Heading>
-                            <p>
-                                Focused on computational and theoretical physics
-                                as well as not-very-applied mathematics.
-                            </p>
-                        </TimelineItem>
-                    </Timeline>
-                </section>
-                <section>
-                    <Heading as="h2" className="text--left">
-                        Fun facts
-                    </Heading>
-                    <div className="flex-grow text-center grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
-                        <div
-                            className={clsx("card", styles.card, "shadow--md")}
-                        >
-                            <div className="card__header">
-                                <Heading as={"h3"}>Home</Heading>
-                            </div>
-                            <div className="card__body">
-                                <p>
-                                    I'm currently living in Columbia SC with my
-                                    wife and two cats
-                                </p>
-                            </div>
-                        </div>
-                        <div
-                            className={clsx("card", styles.card, "shadow--md")}
-                        >
-                            <div className="card__header">
-                                <Heading as={"h3"}>Hobbies</Heading>
-                            </div>
-                            <div className="card__body">
-                                <p>
-                                    I enjoy ultimate frisbee, hiking, and
-                                    anything outdoors. I'm thinking about
-                                    starting Jiu Jitsu
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </main>
-        </Layout>
-    );
+            <div className={clsx("card", styles.card, "shadow--md")}>
+              <div className="card__header">
+                <Heading as={"h3"}>Hobbies</Heading>
+              </div>
+              <div className="card__body">
+                <p>
+                  I enjoy ultimate frisbee, hiking, BJJ and anything outdoors.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,77 +6,43 @@ import styles from "./index.module.css";
 import { ReactNode } from "react";
 
 function HomepageHeader() {
-    return (
-        <header className="text--center print:hidden">
-            <div className="container">
-                <div className="avatar avatar--vertical">
-                    <img
-                        className={clsx(
-                            styles.mainAvatar,
-                            "avatar__photo margin--md"
-                        )}
-                        alt="Scott Driggers"
-                        src="https://gravatar.com/avatar/c7abdf73e309877ecf09e03f27d44a4530dbb98035e47bd86b001a396d095a9b?size=2048"
-                    />
-                </div>
-                <Heading as="h2" className="hero__title">
-                    Scott Driggers
-                </Heading>
-                {/* <p className="hero__subtitle">Welcome to my site</p> */}
-            </div>
-        </header>
-    );
+  return (
+    <header className="text--center print:hidden">
+      <div className="container">
+        <div className="avatar avatar--vertical">
+          <img
+            className={clsx(styles.mainAvatar, "avatar__photo margin--md")}
+            alt="Scott Driggers"
+            src="https://gravatar.com/avatar/c7abdf73e309877ecf09e03f27d44a4530dbb98035e47bd86b001a396d095a9b?size=2048"
+          />
+        </div>
+        <Heading as="h2" className="hero__title">
+          Scott Driggers
+        </Heading>
+      </div>
+    </header>
+  );
 }
 
 export default function Home(): ReactNode {
-    return (
-        <Layout
-            title={`Hello from Scott Driggers`}
-            description="Personal webpage for Scott Driggers"
-        >
-            <div className={styles.mainSectionWrapper}>
-                <HomepageHeader />
+  return (
+    <Layout
+      title={`Hello from Scott Driggers`}
+      description="Personal webpage for Scott Driggers"
+    >
+      <div className={styles.mainSectionWrapper}>
+        <HomepageHeader />
 
-                <main className={styles.main}>
-                    {/* <Heading as="h2" className="text--left">
-                    Fun facts
-                </Heading> */}
-                    <div className="flex-grow px-4 text-center">
-                        {/* <div
-                            className={clsx("card", styles.card, "shadow--md")}
-                        >
-                            <div className="card__body"> */}
-                        <p>
-                            I am a software engineer working on projects in web
-                            development (mostly backend), data science,
-                            networking and mobile app development
-                        </p>
-                        {/* </div> */}
-                        {/* </div> */}
-                        {/* <div className={clsx("card", styles.card, "shadow--md")}>
-                        <div className="card__header">
-                            <h3>Home</h3>
-                        </div>
-                        <div className="card__body">
-                            <p>
-                                Living in Columbia SC with my wife and two cats
-                            </p>
-                        </div>
-                    </div>
-                    <div className={clsx("card", styles.card, "shadow--md")}>
-                        <div className="card__header">
-                            <h3>Hobbies</h3>
-                        </div>
-                        <div className="card__body">
-                            <p>
-                                I enjoy ultimate frisbee, hiking, and anything
-                                outdoors.
-                            </p>
-                        </div>
-                    </div> */}
-                    </div>
-                </main>
-            </div>
-        </Layout>
-    );
+        <main className={styles.main}>
+          <div className="grow px-4 text-center">
+            <p>
+              I am a software engineer working on projects in web development
+              (mostly backend), data science, networking and mobile app
+              development
+            </p>
+          </div>
+        </main>
+      </div>
+    </Layout>
+  );
 }
